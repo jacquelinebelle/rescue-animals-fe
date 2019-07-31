f4c5f8fb6ae6d6083a46b9ff2d3be2e9f5d228c4
Jacqueline Moore <jacquelinebmoore@gmail.com>
2019-07-31 10:02:01 -0600

    add messages for loading and error states

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { thunkAnimals } from './api/fetchAnimals';
-import { setAnimals, isLoading, gotError } from './actions';
+import AnimalCards from './components/AnimalCards';
 import './App.css';
 
 export class App extends Component {
@@ -16,7 +16,8 @@ export class App extends Component {
   render() {
     return (
       <div>
-
+        {this.props.loading && <p>please wait while animal data is loading!</p>}
+        {this.props.error && <p>there has been an error retrieving animal data. i'm so sorry.</p>}
       </div>
     )
   }
