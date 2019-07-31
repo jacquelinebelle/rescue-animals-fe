fc98c673bc9cf39c8ae9cab27c074da73ca19898
Jacqueline Moore <jacquelinebmoore@gmail.com>
2019-07-31 10:36:39 -0600

    place inputs and button in form

--- src/containers/DonationForm/index.js
+++ src/containers/DonationForm/index.js
@@ -4,10 +4,20 @@ import './DonationForm.css';
 
 export class DonationForm extends Component {
 
+    handleChange = (e) => {
+        
+    }
+
+    handleSubmit = (e) => {
+
+    }
+
     render() {
         return (
             <form className="donation-form">
-
+                <input className="name-input" type="text" onChange={this.handleChange} />
+                <input className="donation-input" type="number" onChange={this.handleChange} />
+                <button className="submit-donation-btn" onClick={this.handleSubmit}>submit</button>
             </form>
         )
     }
