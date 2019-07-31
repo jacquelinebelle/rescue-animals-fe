031be5376be1e2c758b0be603b13d08263833aaf
Jacqueline Moore <jacquelinebmoore@gmail.com>
2019-07-31 09:41:26 -0600

    create isLoading and gotError actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,13 @@ export const setAnimals = (animals) => ({
     type: 'SET_ANIMALS',
     animals
 });
+
+export const isLoading = (bool) => ({
+    type: 'IS_LOADING',
+    bool
+});
+
+export const gotError = (message) => ({
+    type: 'GOT_ERROR',
+    message
+});
